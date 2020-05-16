eef617c31996861b1adc355c08acb022afe6b65a
cristhian A <cristhianarce30@gmail.com>
2020-05-16 15:09:05 -0500

    v-if,v-for and v-bind implementation

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+new Vue({
+  el:'#app',
 
+  data(){
+    return {
+      name: 'Bitcoin',
+      img: 'https://w7.pngwing.com/pngs/927/841/png-transparent-bitcoin-logo-cryptocurrency-ethereum-bitcoin-text-orange-logo.png',
+      changePercent: 10,
+      prices: [100,1,2,0,-45,700,254]
+    }
+  }
+})
